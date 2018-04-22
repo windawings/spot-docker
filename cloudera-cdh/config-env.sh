a287fefcd0f1994c5281c9311e2e87fbb1cffbb2
windawings <windawings@foxmail.com>
2018-04-22 13:37:16 +0800

    fix missing install items

--- cloudera-cdh/config-env.sh
+++ cloudera-cdh/config-env.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # yum install
-yum makecache && yum -y update && yum -y upgrade
-yum -y install java-1.8.0-openjdk-devel hadoop zookeeper wget python-pip ntp
-yum clean all && rm -rf /var/cache/yum/*
+yum -y install java-1.8.0-openjdk-devel epel-release wget ntp
+yum -y update && yum -y upgrade
 
 # get cloudera.repo
 mkdir /cloudera_init
@@ -11,10 +10,16 @@ cd /cloudera_init
 wget http://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/cloudera-cdh5.repo -O /etc/yum.repos.d/cloudera-cdh5.repo
 wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo -O /etc/yum.repos.d/cloudera-mng.repo
 sed -i.bak s/^.*"baseurl".*/"baseurl=http:\/\/archive.cloudera.com\/cdh5\/redhat\/7\/x86_64\/cdh\/$CDH_VER\/"/ /etc/yum.repos.d/cloudera-cdh5.repo
-sed -i.bak s/^.*"baseurl".*/"baseurl=http:\/\/archive.cloudera.com\/cm5\/redhat\/7\/x86_64\/cm\/$CM_VER\/"/ /etc/yum.repos.d/cloudera-manager.repo
+sed -i.bak s/^.*"baseurl".*/"baseurl=http:\/\/archive.cloudera.com\/cm5\/redhat\/7\/x86_64\/cm\/$CM_VER\/"/ /etc/yum.repos.d/cloudera-mng.repo
+
+# install hadoop
+yum -y install hadoop zookeeper  python-pip 
+yum clean all
+rm -rf /var/cache/yum/*
 
 # install supervisor
-pip install --upgrade pip && pip install supervisor
+pip install --upgrade pip
+pip install supervisor
 
 # create dir
 mkdir -p /etc/supervisor/conf.d/
