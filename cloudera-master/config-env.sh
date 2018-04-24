4557af7299e8561ca65512c97227573ffc40249c
windawings <windawings@foxmail.com>
2018-04-24 21:14:23 +0800

    add quiet

--- cloudera-master/config-env.sh
+++ cloudera-master/config-env.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # install mysql
-yum install -y wget
-wget http://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-yum localinstall -y mysql57-community-release-el7-11.noarch.rpm
-yum install -y mysql-community-server
+yum install -q -y wget
+wget -q http://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+yum localinstall -qy mysql57-community-release-el7-11.noarch.rpm
+yum install -q -y mysql-community-server
 
 # copy mysql connector
 cp /usr/java/latest/mysql-connector-java.jar /opt/cm/share/cmf/lib/
 
 # config mysql
+mysqld --initialize --console -C utf8
 sed -i /^"\[mysqld\]"/a\\"skip-grant-tables" /etc/my.cnf
-systemctl restart mysqld
+systemctl start mysqld
+#mysqld --daemonize --p
 mysql -u root
 create database scm DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 create database hive DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
@@ -20,13 +22,15 @@ create database hue DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 create database monitor DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 create database oozie DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 grant all on *.* to 'scm'@'localhost' identified by 'temp' with grant option;
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('cloudera');
 exit;
 /opt/cm/cm/share/cmf/schema/scm_prepare_database.sh  mysql cm -h localhost -u scm -p temp --scm-host localhost scm scm scm
 sed -i /^"skip-grant-tables/d" /etc/my.cnf
+systemctl reload mysqld
 systemctl restart mysqld
 
 # clean
-yum remove -y wget
+yum remove -q -y wget
 yum clean all
 rm -rf /var/cache/yum/*
 rm -f mysql57-community-release-el7-11.noarch.rpm
--- cloudera-node/config-env.sh
+++ cloudera-node/config-env.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # yum install
-yum -y update && yum -y upgrade
-yum -y install epel-release wget ntp openssh*
-yum -y install python-pip rpcbind
+yum -q -y update && yum -q -y upgrade
+yum -q -y install epel-release wget ntp openssh*
+yum -q -y install python-pip python-devel rpcbind
 
 # get cloudera repo
-wget http://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/cloudera-cdh5.repo -O /etc/yum.repos.d/cloudera-cdh5.repo
-wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo -O /etc/yum.repos.d/cloudera-mng.repo
+wget -q http://archive.cloudera.com/cdh5/redhat/7/x86_64/cdh/cloudera-cdh5.repo -O /etc/yum.repos.d/cloudera-cdh5.repo
+wget -q http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/cloudera-manager.repo -O /etc/yum.repos.d/cloudera-mng.repo
 sed -i.bak s/^.*"baseurl".*/"baseurl=http:\/\/archive.cloudera.com\/cdh5\/redhat\/7\/x86_64\/cdh\/$CDH_VER\/"/ /etc/yum.repos.d/cloudera-cdh5.repo
 sed -i.bak s/^.*"baseurl".*/"baseurl=http:\/\/archive.cloudera.com\/cm5\/redhat\/7\/x86_64\/cm\/$CM_VER\/"/ /etc/yum.repos.d/cloudera-mng.repo
 
 # install jdk
-wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm -O jdk.rpm
+wget -q --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/jdk-8u172-linux-x64.rpm -O jdk.rpm
 rpm -i jdk.rpm
-wget http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.46/mysql-connector-java-5.1.46.jar -O mysql-connector-java.jar
+wget -q http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.46/mysql-connector-java-5.1.46.jar -O mysql-connector-java.jar
 mv mysql-connector-java.jar /usr/java/latest/
 
 # install cloudera
-wget http://archive.cloudera.com/cm5/cm/5/cloudera-manager-centos7-cm5.14.2_x86_64.tar.gz -O cm.tar.gz
+wget -q http://archive.cloudera.com/cm5/cm/5/cloudera-manager-centos7-cm5.14.2_x86_64.tar.gz -O cm.tar.gz
 tar zxvf cm.tar.gz -C /opt/
 mv /opt/cm-5.14.2 /opt/cm
+sed -i s/^.*"cloudera_mysql_connector_jar".*/"cloudera_mysql_connector_jar=\/usr\/java\/latest\/mysql-connector-java.jar"/ /opt/cm/etc/cloudera-scm-agent/config.ini
 
 # install supervisor
-pip install --upgrade pip
-pip install supervisor
+pip install -q --upgrade pip
+pip install -q supervisor
 
 # config centos
 chmod +x /etc/rc.d/rc.local
@@ -35,7 +36,7 @@ echo never > /sys/kernel/mm/transparent_hugepage/defrag
 
 # clean
 yum clean all
-yum -y remove wget
+yum -q -y remove wget
 rm -rf /var/cache/yum/*
 rm -f jdk.rpm config-env.sh cm.tar.gz
 
