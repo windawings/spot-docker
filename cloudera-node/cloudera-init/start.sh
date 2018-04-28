cbef2fe8909a7ae63a27fa947b7527e4fe395350
windawings <windawings@foxmail.com>
2018-04-28 15:21:17 +0800

    update order for agent start and stop supervisor

--- cloudera-node/cloudera-init/start.sh
+++ cloudera-node/cloudera-init/start.sh
@@ -7,8 +7,8 @@ rm -rf /opt/cm/etc/log/cloudera-scm-agent/*
 # crontab
 crontab /opt/cm/etc/cloudera-scm-agent/cron.tab
 
-# stop supervisor
-systemctl stop supervisord &
-
 # start agent
-/opt/cm/etc/init.d/cloudera-scm-agent start
+/opt/cm/etc/init.d/cloudera-scm-agent start &
+
+# stop supervisor
+systemctl stop supervisord
