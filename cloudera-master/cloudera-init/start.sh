7dd67ad56e36174fa2c8af8475c6e66aebf39578
windawings <windawings@foxmail.com>
2018-04-30 15:45:15 +0800

    add log for start shell

--- cloudera-master/cloudera-init/start.sh
+++ cloudera-master/cloudera-init/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # config mysql
+echo "[+] config cloudera mysql"
 sed -i /^"\[mysqld\]"/a\\"character-set-server=utf8" /etc/my.cnf
 sed -i /^"\[mysqld\]"/a\\"skip-grant-tables" /etc/my.cnf
 systemctl restart mysqld
@@ -10,13 +11,16 @@ systemctl restart mysqld
 /opt/cm/share/cmf/schema/scm_prepare_database.sh mysql scm scm temp
 
 # config hostname
+echo "[+] config k8s hostname"
 echo $HOSTNAME > /etc/hostname
 if [ $(grep -c $HOSTNAME /etc/hosts) -eq 0 ]; then
   echo -e "${POD_IP} ${HOSTNAME}" >> /etc/hosts
-  reboot
+  echo "[+] reboot"
+  shutdown -r now
 fi
 
 # check dir for k8s sa
+echo "[+] fix k8s serviceaccount mount error"
 if [ ! -d "/run/secrets/kubernetes.io/serviceaccount" ]; then
   mkdir -p /run/secrets/kubernetes.io/serviceaccount
 fi
--- cloudera-node/cloudera-init/start.sh
+++ cloudera-node/cloudera-init/start.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 # crontab
+echo "[+] config ntp crontab task"
 crontab /opt/cm/etc/cloudera-scm-agent/cron.tab
 
 # init agent
+echo "[+] init cloudera requirements"
 echo 0 > /proc/sys/vm/swappiness
 echo never > /sys/kernel/mm/transparent_hugepage/defrag
 echo never > /sys/kernel/mm/transparent_hugepage/enabled
 
 # stop agent
+echo "[+] clean agent history"
 bash /cloudera-init/run/stop.sh
 rm -rf /opt/cm/etc/log/cloudera-scm-agent/*
 
 # check dir for k8s sa
+echo "[+] fix k8s serviceaccount mount error"
 if [ ! -d "/run/secrets/kubernetes.io/serviceaccount" ]; then
   mkdir -p /run/secrets/kubernetes.io/serviceaccount
 fi
