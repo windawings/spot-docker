a0c599fc82983081c2d7e1aa13473b277db4ed76
windawings <windawings@foxmail.com>
2018-05-02 16:05:43 +0800

    cannot config mysql in docker build

--- cloudera-master/cloudera-init/start.sh
+++ cloudera-master/cloudera-init/start.sh
@@ -4,14 +4,20 @@
 echo "[+] $(date) check cloudera mysql"
 while [ $(ps -ef | grep mysqld | egrep -v grep | wc -l) -eq 0 ]; do
   echo "[-] $(date) mysqld is not running"
+  systemctl start mysqld
   sleep 2s
 done
 
 # config mysql
 if [ $(grep -c "character-set-server=utf8" /etc/my.cnf) -eq 0 ]; then
-  sleep 20s # avoid mysqld service restart too fast
   echo "[+] $(date) config cloudera mysql"
+  sleep 15s # avoid mysqld service restart too fast
+  sed -i /^"\[mysqld\]"/a\\"skip-grant-tables" /etc/my.cnf
   sed -i /^"\[mysqld\]"/a\\"character-set-server=utf8" /etc/my.cnf
+  systemctl restart mysqld
+  mysql -u root < /cloudera-init/run/mysql.sql
+  sed -i /^"skip-grant-tables/d" /etc/my.cnf
+  systemctl restart mysqld
   /opt/cm/share/cmf/schema/scm_prepare_database.sh mysql scm scm temp
 fi
 
