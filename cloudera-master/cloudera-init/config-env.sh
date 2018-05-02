4865f52370a06760441de9162d8103c0b1e47f71
windawings <windawings@foxmail.com>
2018-05-02 16:39:54 +0800

    well, mysql cannot run now

--- cloudera-master/cloudera-init/config-env.sh
+++ cloudera-master/cloudera-init/config-env.sh
@@ -2,8 +2,8 @@
 
 # install mysql
 yum install -q -y wget
-wget -q http://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-yum localinstall -q -y mysql57-community-release-el7-11.noarch.rpm
+wget -q https://repo.mysql.com/mysql57-community-release-el7.rpm
+yum localinstall -q -y mysql57-community-release-el7.rpm
 yum install -q -y mysql-community-server
 
 # copy mysql connector
@@ -11,10 +11,11 @@ cp /usr/java/latest/mysql-connector-java.jar /opt/cm/share/cmf/lib/
 
 # config autostart
 chkconfig ntpd on
+chkconfig mysqld on
 
 # clean
 yum remove -q -y wget
 yum clean all
 rm -rf /var/cache/yum/*
 rm -rf /etc/yum.repo.d/mysql-community*
-rm -f mysql57-community-release-el7-11.noarch.rpm /cloudera-init/run/config-env.sh
+rm -f mysql57-community-release-el7.rpm /cloudera-init/run/config-env.sh
