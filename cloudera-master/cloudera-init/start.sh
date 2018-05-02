a66996672f3c60e56f832a828a83d83caeafe900
windawings <windawings@foxmail.com>
2018-05-02 13:13:38 +0800

    still cannot read new environment

--- cloudera-master/cloudera-init/start.sh
+++ cloudera-master/cloudera-init/start.sh
@@ -1,44 +1,36 @@
 #!/bin/bash
 
 # check environment
-if [ ! -n "$POD_NAME" ]; then
+if [ -n "$POD_NAME" ]; then
+  # config hostname
+  echo "[+] $(date) config k8s hostname"
+
+  echo "[+] $(date) check k8s hostname file"
+  while [ ! -e /etc/hostname -o ! -e /etc/hosts ]; do
+    echo "[-] $(date) hostname or hosts not found"
+    sleep 2s
+  done
+
+  # try anything i can to update hostname, even though i open the port 4434 in service
+  if [ $(grep -c $HOSTNAME /etc/hostname) -eq 0 ]; then
+    hostname $HOSTNAME
+    echo $HOSTNAME > /etc/hostname
+    hostnamectl set-hostname $HOSTNAME
+  fi
+
+  if [ ! -e /etc/sysconfig/network ]; then
+    touch /etc/sysconfig/network
+  fi
+
+  if [ $(grep -c $HOSTNAME /etc/sysconfig/network) -eq 0 ]; then
+    echo -e "HOSTNAME=${HOSTNAME}" >> /etc/sysconfig/network
+  fi
+
+  if [ $(grep -c $HOSTNAME /etc/hosts) -eq 0 ]; then
+    sed -i "s/^$POD_IP.*$POD_NAME$/& $HOSTNAME/g" /etc/hosts
+  fi
+else
   echo "[-] $(date) invalid environment: pod name is null"
-  # crontab
-  crontab /cloudera-init/run/cron.tab
-  exit 1
-fi
-
-# clean crontabs
-crontab -r
-
-# config hostname
-echo "[+] $(date) config k8s hostname"
-
-echo "[+] $(date) check k8s hostname file"
-while [ ! -e /etc/hostname -o ! -e /etc/hosts ]; do
-  echo "[-] $(date) hostname or hosts not found"
-  sleep 2s
-done
-
-# try anything i can to update hostname, even though i open the port 4434 in service
-if [ $(grep -c $HOSTNAME /etc/hostname) -eq 0 ]; then
-  hostname $HOSTNAME
-  echo $HOSTNAME > /etc/hostname
-  hostnamectl set-hostname $HOSTNAME
-fi
-
-if [ ! -e /etc/sysconfig/network ]; then
-  touch /etc/sysconfig/network
-fi
-
-if [ $(grep -c $HOSTNAME /etc/sysconfig/network) -eq 0 ]; then
-  echo -e "HOSTNAME=${HOSTNAME}" >> /etc/sysconfig/network
-fi
-
-if [ $(grep -c $HOSTNAME /etc/hosts) -eq 0 ]; then
-  sed -i "s/^$POD_IP.*$POD_NAME$/& $HOSTNAME/g" /etc/hosts
-  echo "[+] $(date) reboot"
-  #shutdown -r now
 fi
 
 # check mysqld
